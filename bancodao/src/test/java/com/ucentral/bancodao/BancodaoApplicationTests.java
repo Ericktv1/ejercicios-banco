143f4ce0e7149b81456a060ff6e5348aa45cfa41
erick <etrujillov1@ucentral.edu.co>
2024-04-24 16:30:56 -0500

    primer commit

---
+++ bancodao/src/test/java/com/ucentral/bancodao/BancodaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucentral.bancodao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancodaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
